c26ca99f0ea531cfcd25e0577a3da742d853201d
AhmedTitef <57777850+AhmedTitef@users.noreply.github.com>
2021-07-30 15:50:01 -0700

    Initial Commit

---
+++ Step Counter/Step_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_CounterApp.swift
+//  Step Counter
+//
+//  Created by Ahmed Elsayed on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Step_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
